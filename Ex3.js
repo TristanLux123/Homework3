1251755a4d31ab7620f21ded7ae2f1afd0db45b2
TristanLux123 <tristan.lux8@gmail.com>
2022-09-28 13:01:16 -0400

    concat

--- Ex3.js
+++ Ex3.js
@@ -1,4 +1,6 @@
 var str1 = "I";
 var str2 = " Love";
 var str3 = " Typescript!";
-console.log(str1.concat(str2, str3.toString()));
+var str4 = (str1 + str2 + str3);
+console.log(str4);
+// console.log(str1.concat(str2,str3.toString())) //More productive?
--- Ex3.ts
+++ Ex3.ts
@@ -2,8 +2,10 @@ let str1:string = "I";
 let str2:string = " Love";
 let str3:string = " Typescript!";
 
+let str4 = (str1 + str2 + str3)
 
-console.log(str1.concat(str2,str3.toString()))
+console.log(str4)
+// console.log(str1.concat(str2,str3.toString())) //More productive?
 
 
 
